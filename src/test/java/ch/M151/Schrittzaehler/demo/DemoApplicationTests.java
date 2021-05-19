9b24263a9cda0e1b35118a33ef172f13e88d8971
JaniisKn <Janis_Kneubuehler@sluz.ch>
2021-05-19 09:27:44 +0200

    init

---
+++ src/test/java/ch/M151/Schrittzaehler/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.M151.Schrittzaehler.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
