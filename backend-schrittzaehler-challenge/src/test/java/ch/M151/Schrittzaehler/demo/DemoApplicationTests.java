29c8d9d23ea5581e91ef1d9d5689b1a8d84bde84
savogel1 <sacha_vogel@sluz.ch>
2021-05-26 13:21:37 +0200

    change folder structure

--- backend-schrittzaehler-challenge/src/test/java/ch/M151/Schrittzaehler/demo/DemoApplicationTests.java
+++ backend-schrittzaehler-challenge/src/test/java/ch/M151/Schrittzaehler/demo/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package ch.M151.Schrittzaehler.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class DemoApplicationTests {
 
