6e1721a60cf3c4e5c1ce9d49538e1b8903ce1595
savogel1 <sacha_vogel@sluz.ch>
2021-06-02 12:43:40 +0200

    run project with docker containers

---
+++ backend-schrittzaehler-challenge/src/main/java/ch/M151/schrittzaehlerchallenge/SchrittzaehlerChallengeApplication.java
@@ -0,0 +1,11 @@
+package ch.M151.schrittzaehlerchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchrittzaehlerChallengeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchrittzaehlerChallengeApplication.class, args);
+    }
+}
