2524c1d260a858890e33c6c55279d82386078b16
savogel1 <sacha_vogel@sluz.ch>
2021-06-09 08:57:19 +0200

    run all docker containers together

--- backend-schrittzaehler-challenge/src/main/resources/db/migration/V1__Initial.sql
+++ backend-schrittzaehler-challenge/src/main/resources/db/migration/V1__Initial.sql
@@ -1,43 +1,43 @@
 CREATE TYPE competitionStateEnum AS ENUM ('running', 'finished', 'pending');
 
-CREATE TABLE public.competition {
+CREATE TABLE public.competition (
     id bigint NOT NULL,
     startDate date NULL,
     endDate date NULL,
     state competitionStateEnum NOT NULL,
     CONSTRAINT pk_competition PRIMARY KEY (id)
-}
+);
 
-CREATE TABLE public.group {
+CREATE TABLE public.groups (
     id bigint NOT NULL,
     competition_id bigint NOT NULL,
     groupname varchar(255) NOT NULL,
-    CONSTRAINT pk_group PRIMARY KEY (id),
+    CONSTRAINT pk_groups PRIMARY KEY (id),
     CONSTRAINT fk_competition_id FOREIGN KEY (competition_id)
         REFERENCES competition (id)
-}
+);
 
 CREATE TYPE userRoleEnum AS ENUM ('admin', 'normal');
 
-CREATE TABLE public.user {
+CREATE TABLE public.users (
     id bigint NOT NULL,
-    group_id bigint NOT NULL,
+    groups_id bigint NOT NULL,
     username varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
     dailyStepGoal INT NULL,
     userRole userRoleEnum NOT NULL,
-    CONSTRAINT pk_user PRIMARY KEY (id),
-    CONSTRAINT fk_group_id FOREIGN KEY (group_id)
-        REFERENCES group (id)
-}
+    CONSTRAINT pk_users PRIMARY KEY (id),
+    CONSTRAINT fk_groups_id FOREIGN KEY (groups_id)
+        REFERENCES groups (id)
+);
 
-CREATE TABLE public.steps {
+CREATE TABLE public.steps (
     id bigint NOT NULL,
-    user_id bigint NOT NULL,
+    users_id bigint NOT NULL,
     numberOfSteps INT NOT NULL,
     creationDate date NOT NULL,
     CONSTRAINT pk_steps PRIMARY KEY (id),
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id)
-        REFERENCES user (id)
-}
+    CONSTRAINT fk_users_id FOREIGN KEY (users_id)
+        REFERENCES users (id)
+);
